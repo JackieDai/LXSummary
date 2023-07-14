1151e3df1722305064b72d863db0f3fac375c0e6
代零霄 <lingxiao@infinit.com.sg>
2023-07-14 10:41:26 +0800

    modify swift compiler custom flag upon building setting

--- LXSummary/ViewController.swift
+++ LXSummary/ViewController.swift
@@ -11,6 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+#if DEBUG && PROD
+        print("DEBUG && PROD")
+        view.backgroundColor = .red
+#elseif DEBUG && UAT
+        print("DEBUG && UAT")
+        view.backgroundColor = .green
+#elseif UAT
+        print("UAT release")
+        view.backgroundColor = .blue
+#elseif PROD
+        view.backgroundColor = .yellow
+        print("AppStore release")
+#endif
     }
 }
 
